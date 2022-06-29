3f65c4d9e01ed618089dda2e52cb3fdbf7700822
narayanmane <88097660+narayanmane@users.noreply.github.com>
2022-06-29 14:43:59 +0530

    SMOL

---
+++ Basic programming/Smallest Possible Whole Number .cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	       int n,k;
+	       int x=0;
+	       cin>>n>>k;
+	      if(k==0)
+	      {
+	             cout<<n<<endl;
+	      }
+	      else
+	      {
+	             cout<<n%k<<endl;
+	      }
+	}
+	return 0;
+}
