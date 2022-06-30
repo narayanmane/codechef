e917d2b568679cda18cddc6c5525bbc6b3967adb
narayanmane <88097660+narayanmane@users.noreply.github.com>
2022-06-30 14:33:41 +0530

    MANYSUMS

---
+++ Basic programming/Distinct Pair Sums.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	       int a,b;
+	       cin>>a>>b;
+	       int s=b-a;
+	       cout<<2*s+1<<endl;
+	}
+	return 0;
+}
